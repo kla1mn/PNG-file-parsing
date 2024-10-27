e132b2141cd7973bd9187a90036dce5162f81fd8
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-10-27 16:20:22 +0500

    Update

--- parser.py
+++ parser.py
@@ -1,13 +1,27 @@
+class Parser:
+    def read_chunk(self, file):
+        length = int.from_bytes(file.read(4), 'big')
+        try:
+            if length == 0:
+                file.read(4 + 4 + 4)
+            else:
+                print(file.read(4))
+                print(file.read(length))
+                file.read(4)
+        except Exception as e:
+            print(e)
+        print(length)
+        input()
 
-
-def parse(file_name: str):
-    with open(file_name, 'rb') as file:
-        signature = file.read(8)
-        if b'PNG' not in signature:
-            return None
-        print('ok')
+    def parse(self, file_name: str):
+        with open(file_name, 'rb') as file:
+            signature = file.read(8)
+            if b'PNG' not in signature:
+                return None
+            while file.readable():
+                chunk = self.read_chunk(file)
 
 
 if __name__ == '__main__':
-    parse("pizza.png")
-    parse("pizza.jpeg")
+    parser = Parser()
+    parser.parse("pizza.png")
