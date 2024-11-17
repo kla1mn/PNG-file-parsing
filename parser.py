0d40b1414dbf68f354b728166d1011f3c26885d2
PurpleCat404 <bednyak43@mail.ru>
2024-11-17 17:12:46 +0500

    Renaming + 55% decompress_data
    check comments in the code

--- parser.py
+++ parser.py
@@ -1,12 +1,17 @@
+import zlib
+from typing import List
 from chunk import Chunk
 from ihdr_information import IHDRInformation
-from pixel import Pixel
-
+from plte_information import PLTEInformation
+import tkinter as tk
 
 class Parser:
     def __init__(self):
         self.compressed_data_idat = b''
         self.chunks = []
+        self.ihdr_information = None
+        self.palette: List[PLTEInformation] = []
+        self.raw_image = None
 
     def read_chunks(self, file):
         while True:
@@ -47,6 +52,9 @@ class Parser:
                     self.parse_IHDR(chunk)
                 elif chunk.chunk_type == b'IDAT':
                     self.parse_IDAT(chunk)
+                elif chunk.chunk_type == b'PLTE':
+                    self.parse_PLTE(chunk)
+
                 elif chunk.chunk_type == b'IEND':
                     break
 
@@ -61,28 +69,58 @@ class Parser:
         self.compressed_data_idat += chunk.data
 
     def parse_PLTE(self, chunk: Chunk):
-        # TODO
-        # информация о палитре, в некоторых color_type из IHDR необязательна и в чанках PLTE может не быть.
-        # см. https://www.w3.org/TR/png/#11PLTE
 
-
-        #возможно, надо будет переделать...
         if chunk.length % 3 != 0:
             raise ValueError("Not a valid PNG file (the length of the PLTE data is not a multiple of three)")
 
         data = chunk.data
-
-        self.pixels: list(Pixel) = []
+        self.palette = []
 
         for i in range(0, len(data), 3):
-            self.pixels.append(Pixel(i // 3, data[i], data[i + 1], data[i + 2]))
+            self.palette.append(PLTEInformation(i // 3, data[i], data[i + 1], data[i + 2]))
 
     def decompress_data(self):
-        # TODO
-        # фактически, получение всех пикселей для составления изображения
-        pass
+        decompressed_data = zlib.decompress(self.compressed_data_idat)
+
+        bytes_per_pixel = self.get_bytes_per_pixel()
+
+        #Смотрим сколько пикселей в строке и умножаем на число байт, чтобы получить, сколько байт в строке
+        stride = bytes_per_pixel * self.ihdr_information.width
+        self.raw_image = []
+
+        i = 0
+        #добавляем в сырое изображение строку вместе со своим фильтром, чтобы потом применить фильтр к каждой строке
+        for row in range(self.ihdr_information.height):
+            filter_type = decompressed_data[i]
+            i += 1
+            scanline = decompressed_data[i:i + stride]
+            i += stride
+            self.raw_image.append((filter_type, scanline))
+
+        print(f"Parsed {len(self.raw_image)} scanlines.")
+
+        # Применение фильтров для восстановления пиксельных данных
+        # self.apply_filters() TODO
+
+    def get_bytes_per_pixel(self): #переделать бы это на константу-словарь...
+        color_type = self.ihdr_information.color_type
+
+        if color_type == 0:  # Grayscale
+            return 1
+        elif color_type == 2:  # Truecolor
+            return 3
+        elif color_type == 3:  # Indexed-color
+            return 1
+        elif color_type == 4:  # Grayscale with alpha
+            return 2
+        elif color_type == 6:  # Truecolor with alpha
+            return 4
+        else:
+            raise ValueError(f"Unsupported color type: {color_type}")
 
 
 if __name__ == '__main__':
     parser = Parser()
     parser.parse("rgb.png")
+    parser.decompress_data()
+    parser.display_image()
--- plte_information.py
+++ plte_information.py
@@ -1,5 +1,5 @@
-class Pixel:
-    def __init__(self, index, R, G, B):
+class PLTEInformation:
+    def __init__(self, index: int, R: int, G: int, B: int):
         self.index = index
         self.R = R
         self.G = G
