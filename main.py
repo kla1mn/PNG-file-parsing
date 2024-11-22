f139793dbd818bd1f136732f7afa0ea6ff1883d8
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-11-22 14:26:33 +0500

    Update cat 18+

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from parser import Parser
 
 parser = Parser()
-parser.parse("images/originals/pizza.png")
-# parser.parse("images/cat 1950s vibe.png")
+parser.parse("images/originals/rgb.png")
 parser.decompress_data()
 parser.display_image()
